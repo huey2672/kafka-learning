0ec8c67c98b1cc6fef61c798cd53689c9c44417c
huey2672 <huey2672@163.com>
2021-07-13 17:17:03 +0800

    quick start

--- kafka-quickstart/src/main/java/com/huey/learning/kafka/quickstart/ConsumerSample.java
+++ kafka-quickstart/src/main/java/com/huey/learning/kafka/quickstart/ConsumerSample.java
@@ -1,5 +1,43 @@
-package com.huey.learning.kafka.quickstart;/**
- * 
+package com.huey.learning.kafka.quickstart;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * @author huey
- */public class ConsumerSample {
+ */
+public class ConsumerSample {
+
+    public static void main(String[] args) {
+
+        //
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "SIMPLE_CONSUMER");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Collections.singletonList("SimpleTopic"));
+
+        ConsumerRecords<String, String> records = null;
+        while (records == null || records.isEmpty()) {
+            System.out.println("Waiting for records.");
+            records = consumer.poll(Duration.ofSeconds(1));
+        }
+
+        for (ConsumerRecord<String, String> record : records) {
+            System.out.println(record.value());
+        }
+
+        consumer.close();
+
+    }
+
 }
--- kafka-quickstart/src/main/java/com/huey/learning/kafka/quickstart/ProducerSample.java
+++ kafka-quickstart/src/main/java/com/huey/learning/kafka/quickstart/ProducerSample.java
@@ -1,5 +1,34 @@
-package com.huey.learning.kafka.quickstart;/**
- * 
+package com.huey.learning.kafka.quickstart;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * @author huey
- */public class ProducerSample {
+ */
+public class ProducerSample {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        ProducerRecord<String, String> record
+                = new ProducerRecord<>("SimpleTopic", "Hello, Kafka!");
+
+        producer.send(record);
+
+        producer.close();
+
+    }
+
 }
